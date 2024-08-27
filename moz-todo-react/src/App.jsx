d6416dea547ab425e09a33e8430101ed174def76
Missbellum <enjaykanz@gmail.com>
2024-08-27 18:37:24 +0100

    First Commit

---
+++ moz-todo-react/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(props) {
+  // prop - passes data into react component
+  // only passed form parent to child
+  console.log(props);
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Chris + Vite + React === Besties</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>
+      
+      <button onClick={(e) => {
+          e.currentTarget.textContent = "I'm clicked!"
+        }} >{ props.subject.toUpperCase() } Me!</button>
+    </>
+  )
+}
+
+export default App
